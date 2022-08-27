535c7d823df18b4dff2d6ece810a29e5554d5133
Kotari Rama Prathyusha <kotaruramaprathyusha@gmail.com>
2022-08-27 17:49:26 -0500

    c1

---
+++ test.sh
@@ -0,0 +1,4 @@
+Created empty GITHUB repo and cloned locally.
+added text file using shell command and upating the txt file with info about the task
+after this supposed to create feature branch and commit the file.
+
